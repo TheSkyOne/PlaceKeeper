1b6e39b18fc951052ca7f530ad9d3e9ae23ce121
TheSkyOne <42645782+TheSkyOne@users.noreply.github.com>
2025-01-27 18:40:16 +0200

    place.service.js changed to used the storage service

--- js/services/place.service.js
+++ js/services/place.service.js
@@ -1,25 +1,37 @@
-import { genId } from "./utils.service.js"
+import { genId, saveToStorage, loadFromStorage } from "./utils.service.js"
+import { storageService } from "./storage.service.js"
 
 export const placeService = {
     getPlaces,
+    addPlace,
     removePlace
 }
 
+const SOTRAGE_KEY = "places"
 
 let gPlaces = []
 
 _createPlaces()
 
 
-function removePlace(placeId){
-    var placeIdx = _getPlaceIndexById(placeId)
-    gPlaces.splice(placeIdx, 1)
-}
-
-
 function getPlaces(){
-    return gPlaces
+    return storageService.query(SOTRAGE_KEY)
 }
+
+
+function addPlace(lat, lng, name, zoom){
+    var newPlace = _createPlace(lat, lng, name)
+    return storageService.post(SOTRAGE_KEY, newPlace)
+    // gPlaces.push(newPlace)
+}
+
+
+function removePlace(placeId){
+    return storageService.remove(SOTRAGE_KEY, placeId)
+    // var placeIdx = _getPlaceIndexById(placeId)
+    // gPlaces.splice(placeIdx, 1)
+}
+
 
 
 function _createPlace(lat, lng, name){
@@ -33,15 +45,16 @@ function _createPlace(lat, lng, name){
 
 
 function _createPlaces(){
+    gPlaces = loadFromStorage(SOTRAGE_KEY) || []
+    if(gPlaces && gPlaces.length) return // already had places in storage
+
     var lat = 1
     var lng = 1
     for(let i = 0; i < 5; i++){
-        let place = _createPlace(lat, lng, `name${lat}${lng}`)
-        gPlaces.push(place)
-
-        lat++
-        lng++
+        gPlaces.push(_createPlace(lat, lng, `demo place ${i}`))
     }
+
+    saveToStorage(SOTRAGE_KEY, gPlaces)
 
 }
 
